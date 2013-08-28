6650302382d38dc08ee9c30abeff67c2eb4ba3e5
Boo-urns <coreyaburns@gmail.com>
2013-08-28 15:40:24 -0500

    Dynamic values with raw_input

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,12 @@
-meal_cost = 20
-taxRate = 15
-tipRate = 15
+meal_cost = float(raw_input("Cost of your meal: "))
+taxRate = float(raw_input("What is your tax rate percentage? "))
+tipRate = float(raw_input("What percentage do you want to tip? "))
 
 tax = meal_cost * taxRate/100
 meal_w_tax = meal_cost + tax
 
 # you should tip on pre-taxed meal! 
 tip = meal_cost * tipRate/100
-
 total = meal_w_tax + tip
 
 print "The base cost of of your meal was $%.2f" % meal_cost
