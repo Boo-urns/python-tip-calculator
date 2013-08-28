66dff9c6f3827b370c4a113c62cccedfd2b4151f
Boo-urns <coreyaburns@gmail.com>
2013-08-28 14:57:03 -0500

    Python Tip Calculator - Static Values

---
+++ tip_calculator.py
@@ -0,0 +1,16 @@
+meal_cost = 20
+taxRate = 15
+tipRate = 15
+
+tax = meal_cost * taxRate/100
+meal_w_tax = meal_cost + tax
+
+# you should tip on pre-taxed meal! 
+tip = meal_cost * tipRate/100
+
+total = meal_w_tax + tip
+
+print "The base cost of of your meal was $%.2f" % meal_cost
+print "You need to pay $%.2f for tax" % tax
+print "Tipping at a rate of %d%% you should leave $%.2f for a tip" % (tipRate, tip)
+print "The grand total of your meal is $%.2f" % total
